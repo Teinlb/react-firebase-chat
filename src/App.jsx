43fae38e9a4365b8e175349a8acce04296a2cd35
teinlb <tein.lebouille@gmail.com>
2024-08-20 10:04:14 +0200

    modified:   src/App.jsx 	modified:   src/components/chat/Chat.jsx 	modified:   src/components/chat/chat.css 	modified:   src/components/detail/Detail.jsx 	modified:   src/components/detail/detail.css 	modified:   src/components/list/List.jsx 	new file:   src/components/list/chatList/ChatList.jsx 	new file:   src/components/list/chatList/chatList.css 	modified:   src/components/list/list.css 	new file:   src/components/list/userInfo/UserInfo.jsx 	new file:   src/components/list/userInfo/userInfo.css 	modified:   src/index.css 	modified:   src/main.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,13 @@ import Chat from "./components/chat/Chat";
 import Detail from "./components/detail/Detail";
 
 const App = () => {
-  return (
-    <div className="container">
-      <List />
-      <Chat />
-      <Detail />
-    </div>
-  );
+    return (
+        <div className="container">
+            <List />
+            <Chat />
+            <Detail />
+        </div>
+    );
 };
 
 export default App;
